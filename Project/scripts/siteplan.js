ee2635a05210412461830be743866dca69b82939
Myamiki <146244292+Myamiki@users.noreply.github.com>
2024-05-28 18:37:18 +0200

    Add files via upload

---
+++ Project/scripts/siteplan.js
@@ -0,0 +1,7 @@
+
+const date = new Date();
+let currentyear = date.getFullYear();
+document.getElementById("currentyear").innerHTML = currentyear;
+
+let lastModified = document.lastModified;
+document.getElementById("lastModified").innerHTML = lastModified;
