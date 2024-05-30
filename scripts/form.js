142c9018460836435a15506f42f3814db5aac880
Myamiki <146244292+Myamiki@users.noreply.github.com>
2024-05-30 22:10:16 +0200

    Update form.js

--- scripts/form.js
+++ scripts/form.js
@@ -7,52 +7,58 @@ document.getElementById("lastModified").innerHTML = lastModified;
 
 const products = [
     {
-      id: fc-1888,
+      id: "fc-1888",
       name: "flux capacitor",
       avgRating: 4.5
     },
 
     {
-      id: fc-2050,
+      id: "fc-2050",
       name: "power laces",
       averagerating: 4.7
     },
     {
-      id: fs-1987,
+      id: "fs-1987",
       name: "time circuits",
       averagerating: 3.5
     },
     {
-      id: ac-2000,
+      id: "ac-2000",
       name: "low voltage reactor",
       averagerating: 3.9
     },
     {
-      id: jj-1969,
+      id: "jj-1969",
       name: "warp equalizer",
       averagerating: 5.0
     }
   ];
 
-  //review form//
+  try {
+    let option = document.getElementById("products").innerHTML;
+    for (const product of products) {
+      option+= '<option value="'+ product.id +'">' + product.name + "</option>"
+    }
+    document.getElementById("products").innerHTML = option;
+  } catch {}
+  
 
-  document.addEventListener('DOMContentLoaded',
-    function(){
-    const reviewForm = document.querySelector('#reviewForm');
-    const counterKey = 'reviewsCompleted';
 
-    let reviewsCompleted = localStorage.getItem(counterKey) || 0;
-   console.log(`Number of reviews completed:${reviewsCompleted}`);
-   reviewForm.addEventListener('submit',
-    function(event){
-    event.preventDefault();
 
-    //Perform from submission actions here//
-   reviewsCompleted++;
-  localStorage.setItem(counterKey, reviewsCompleted);
-  console.log(`Number of reviews completed update:${reviewsCompleted}`);
-});
-    });
+  const counterKey = 'reviewsCompleted';
+  let reviewsCompleted = localStorage.getItem(counterKey) || 0;
+
+  
+
+  document.getElementById('reviewCounter').innerHTML = reviewsCompleted;
+
+  //review form
+
+  function myFunction() {
+    reviewsCompleted = parseInt(reviewsCompleted) + 1;
+
+    localStorage.setItem(counterKey, reviewsCompleted)
+  }
 
     
     //JavaScript code to handle date formatting and validation
@@ -76,7 +82,6 @@ const products = [
       return regex.test(dateString);
     }
   });
-
 
 
 
